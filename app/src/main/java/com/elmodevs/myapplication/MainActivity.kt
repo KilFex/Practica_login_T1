2cef041ec7787ffb0030323cd6deffe5b22a4dc2
KilFex <elivan.1251996@gmail.com>
2026-01-31 01:04:05 -0500

    finish login

--- app/src/main/java/com/elmodevs/myapplication/MainActivity.kt
+++ app/src/main/java/com/elmodevs/myapplication/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.elmodevs.myapplication
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -15,5 +16,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.ingresar.setOnClickListener{
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+        }
     }
 }
